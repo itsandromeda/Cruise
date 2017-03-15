2deaa1e57a2d642c077243e2cdd2d6b0d525b32a
itsandromeda <itsandromeda@gmail.com>
2017-03-15 10:03:46 -0500

    Navbar changes

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,20 +1,21 @@
-/*global document, removeItem*/
-var array = ["Ubuntu", "Firefox3", "Core-Duo"],
+/*global document, removeItem, removeItem2*/
+/*
+var idle1 = ["Ubuntu", "Firefox3", "Core-Duo"],
     span,
     text,
     remove,
     icon;
 
-function print(array) {
+function print(idle1) {
     "use strict";
-    var rsrcs = document.getElementById("serie"),
+    var rsrcs = document.getElementById("serie1"),
         i;
     rsrcs.innerHTML = "";
-    for (i = 0; i < array.length; i += 1) {
+    for (i = 0; i < idle1.length; i += 1) {
         span = document.createElement("span");
         span.setAttribute("class", "resources");
         span.setAttribute("data-re", i);
-        text = document.createTextNode(array[i]);
+        text = document.createTextNode(idle1[i]);
         span.appendChild(text);
         remove = document.createElement("button");
         remove.setAttribute("class", "delete-btn");
@@ -24,6 +25,58 @@ function print(array) {
         span.appendChild(remove);
         rsrcs.appendChild(span);
         remove.addEventListener("click", removeItem);
+    }
+}
+
+function removeItem(event) {
+    "use strict";
+    var element = event.target,
+        parent = (element.tagName === "I") ? element.parentElement.parentElement : element.parentElement,
+        re = parent.getAttribute("data-re");
+    idle1.splice(re, 1);
+    print(idle1);
+}
+print(idle1);
+*/
+/*global document, removeItem*/
+
+var array = [
+    ["Ubuntu", "Firefox3", "Core-Duo"],
+    ["Ubuntu", "Firefox3", "MySQL", "Core-Duo"],
+    ["Ubuntu", "Firefox3", "MySQL", "Core-Duo"],
+    ["Ubuntu"]
+],
+    span,
+    text,
+    remove,
+    icon;
+
+function print(array) {
+    "use strict";
+    var rsrcs = document.getElementById("serie1"),
+        rsrcs2 = document.getElementById("serie2"),
+        i,
+        arr,
+        j,
+        k;
+    rsrcs.innerHTML = "";
+    for (i = 0; i < array[0].length; i += 1) {
+        arr = array[i];
+        for (j = i; j < arr[j].length; j += 1) {
+            span = document.createElement("span");
+            span.setAttribute("class", "resources");
+            span.setAttribute("data-re", i);
+            text = document.createTextNode(arr[j]);
+            span.appendChild(text);
+            remove = document.createElement("button");
+            remove.setAttribute("class", "delete-btn");
+            icon = document.createElement("i");
+            icon.setAttribute("class", "fa fa-trash");
+            remove.appendChild(icon);
+            span.appendChild(remove);
+            rsrcs.appendChild(span);
+            remove.addEventListener("click", removeItem);
+        }
     }
 }
 
